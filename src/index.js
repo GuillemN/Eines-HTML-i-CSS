ddd95ff44d7bb723b7bb8d1a620a0526c6ced46c
Guillem <guillemnaba@gmail.com>
2025-05-04 12:18:50 +0200

    intent millora pagespeed 1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import "./styles/main.scss";
 import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
-
 import "swiper/modules/navigation.css";
 import "swiper/modules/pagination.css";
 
 document.addEventListener("DOMContentLoaded", function () {
+  // Swiper
   const swiper = new Swiper(".swiper", {
-    modules: [Navigation, Pagination], // Registrem els mòduls
-    loop: true, // Activa repetició d'imatges
-    slidesPerView: 1, // Mostra una imatge a la vegada
-    centeredSlides: true, // Centra els slides
+    modules: [Navigation, Pagination],
+    loop: true,
+    slidesPerView: 1,
+    centeredSlides: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -20,4 +20,21 @@ document.addEventListener("DOMContentLoaded", function () {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  // YouTube Lazy Load
+  const loadBtn = document.getElementById("load-video");
+  if (loadBtn) {
+    loadBtn.addEventListener("click", () => {
+      const iframe = document.createElement("iframe");
+      iframe.width = "560";
+      iframe.height = "315";
+      iframe.src = "https://www.youtube.com/embed/CmnHfK4JkGw?si=NMmzj27kLp4FAILm&autoplay=1";
+      iframe.title = "YouTube video player";
+      iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+      iframe.referrerPolicy = "strict-origin-when-cross-origin";
+      iframe.allowFullscreen = true;
+      iframe.classList.add("video-embed");
+      loadBtn.replaceWith(iframe);
+    });
+  }
 });
